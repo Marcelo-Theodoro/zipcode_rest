e153b26155ddf444c23fa035dc926a94c9e62f30
Marcelo Theodoro <marcelotheodoro@outlook.com>
2016-11-11 11:34:50 -0200

    Adicionado models

--- zipcode_rest/zipcode/models.py
+++ zipcode_rest/zipcode/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+UNIDADES_FEDERATIVAS = (
+    ('AC', 'AC'),
+    ('AL', 'AL'),
+    ('AP', 'AP'),
+    ('AM', 'AM'),
+    ('BA', 'BA'),
+    ('CE', 'CE'),
+    ('DF', 'DF'),
+    ('ES', 'ES'),
+    ('GO', 'GO'),
+    ('MA', 'MA'),
+    ('MT', 'MT'),
+    ('MS', 'MS'),
+    ('MG', 'MG'),
+    ('PR', 'PR'),
+    ('PB', 'PB'),
+    ('PA', 'PA'),
+    ('PE', 'PE'),
+    ('PI', 'PI'),
+    ('RJ', 'RJ'),
+    ('RN', 'RN'),
+    ('RS', 'RS'),
+    ('RO', 'RO'),
+    ('RR', 'RR'),
+    ('SC', 'SC'),
+    ('SE', 'SE'),
+    ('SP', 'SP'),
+    ('TO', 'TO'),
+    )
+
+
+class Endereco(models.Model):
+    logradouro = models.CharField(max_length=120)
+    bairro = models.CharField(max_length=60)
+    cidade = models.CharField(max_length=60)
+    uf = models.CharField(max_length=2, choices=UNIDADES_FEDERATIVAS)
+    cep = models.CharField(max_length=8)
+
+    def __str__(self):
+        return self.cep
